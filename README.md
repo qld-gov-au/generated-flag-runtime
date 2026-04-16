9beb3d0fac054d00d9cadab186d3a70c592bb35c
william dutton <will.dutt@gmail.com>
2026-04-16 15:02:40 +1000

    Initial commit, This is just a runtime library to hold the Generated flag for JaCoCo, useful for Jaxb2 code generation where it needs to be internal to your own code base

---
+++ README.md
@@ -0,0 +1,3 @@
+## Generated Flag holder
+
+This library holes a runtime annotation class to allow flagging of classes as Generated and skip reporting in JaCoCo
---
+++ src/main/java/au/gov/qld/online/ExcludeFromJacocoGeneratedReport.java
@@ -0,0 +1,14 @@
+package au.gov.qld.online;
+
+import java.lang.annotation.*;
+
+/**
+ * add @au.gov.qld.smartservice.payment.schemas.ExcludeFromJacocoGeneratedReport annotations where required;
+ * useful for JaCoCo (which has built in support), or other style checkers and code coverage tools:
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+public @interface ExcludeFromJacocoGeneratedReport {
+}
